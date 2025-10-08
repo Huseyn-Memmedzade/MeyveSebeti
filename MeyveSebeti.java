769f876d567f7c9df2ff1be5e84ae9b013e8cfea
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-10-08 13:50:13 +0400

    Add files via upload

---
+++ MeyveSebeti.java
@@ -0,0 +1,24 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class MeyveSebeti {
+
+
+    public static void main(String[] args) {
+
+
+        ArrayList<String> meyveler = new ArrayList<>();
+
+
+        meyveler.add("Alma");
+        meyveler.add("Armud");
+        meyveler.add("Portağal");
+
+        System.out.println("Sebetdeki Meyveler : " + meyveler);
+
+
+    }
+
+
+}
